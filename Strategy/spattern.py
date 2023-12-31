88849f9590b5e3b94d84892c26ee9a2beeb71d7b
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-12-31 06:23:37 +0000

    Implemented strategy pattern

---
+++ Strategy/spattern.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from typing import Protocol
+
+# Abstract Base Class (ABC)
+class Shape(Protocol):
+    @abstractmethod
+    def area(self) -> float:
+        pass
+
+    @abstractmethod
+    def perimeter(self) -> float:
+        pass
+
+# Concrete Subclass
+class Rectangle(Shape):
+    def __init__(self, width: float, height: float):
+        self.width = width
+        self.height = height
+
+    def area(self) -> float:
+        return self.width * self.height
+
+    def perimeter(self) -> float:
+        return 2 * (self.width + self.height)
+
+# Concrete Subclass
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+
+    def area(self) -> float:
+        return 3.14 * self.radius * self.radius
+
+    def perimeter(self) -> float:
+        return 2 * 3.14 * self.radius
+
+# Usage
+rectangle = Rectangle(5.0, 7.0)
+circle = Circle(3.0)
+
+print("Rectangle Area:", rectangle.area())
+print("Rectangle Perimeter:", rectangle.perimeter())
+
+print("Circle Area:", circle.area())
+print("Circle Perimeter:", circle.perimeter())
